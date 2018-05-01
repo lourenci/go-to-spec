1c2f9de2137e56171f34fb7a3b795a28acc1f8a8
Leandro Lourenci <leandro@lourenci.com>
2018-05-01 20:51:10 -0300

    Add yarn and format code to standardjs

--- extension.js
+++ extension.js
@@ -1,83 +1,71 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require('vscode')
 const fs = require('fs')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
+function activate (context) {
+  let disposable = vscode.commands.registerCommand('extension.goToSpec', function () {
+    if (!vscode.workspace.name) {
+      return
+    }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.goToSpec', function () {
-        // The code you place here will be executed every time your command is executed
+    const activeFile = vscode.window.activeTextEditor
+    if (!activeFile) {
+      return
+    }
 
-        // Display a message box to the user
+    const openedFilename = activeFile.document.fileName
+    const isCodeFile = /(.*(\/.*\/))(.*)(\.js|\.rb)$/
 
-        if (!vscode.workspace.name) {
-            return
-        }
+    const openedFile = openedFilename.match(isCodeFile)
 
-        const activeFile = vscode.window.activeTextEditor
-        if (!activeFile) {
-            return
-        }
+    if (!openedFile) {
+      return
+    }
 
-        const openedFilename = activeFile.document.fileName
-        const isCodeFile = /(.*(\/.*\/))(.*)(\.js|\.rb)$/
+    const path = openedFile[1]
+    const lastPath = openedFile[2]
+    const filenameWithoutExtension = openedFile[3]
+    const filenameExtension = openedFile[4]
 
-        const openedFile = openedFilename.match(isCodeFile)
+    const isSpecFile = /(\.|_)(spec|test)\./
+    if (!isSpecFile.test(openedFile)) {
+      const sufixSpecs = ['.spec', '.test', '_spec', '_test']
 
-        if (!openedFile) {
-            return
-        }
+      const sufixToOpen = sufixSpecs.map(spec => `${path}${filenameWithoutExtension}${spec}${filenameExtension}`)
+        .filter(spec => fs.existsSync(spec))
+      if (sufixToOpen.length > 0) {
+        vscode.workspace.openTextDocument(vscode.Uri.file(sufixToOpen[0]))
+          .then(vscode.window.showTextDocument)
+      } else {
+        const fileToOpen = `**${lastPath}${filenameWithoutExtension}_spec${filenameExtension}`
+        vscode.workspace.findFiles(fileToOpen, '**/node_modules/**')
+          .then(files => {
+            vscode.workspace.openTextDocument(vscode.Uri.file(files[0].fsPath))
+              .then(vscode.window.showTextDocument)
+          })
+      }
+    } else {
+      const sufixSpecs = ['.spec', '.test', '_spec', '_test']
+      let fileToOpen = openedFilename
+      sufixSpecs.forEach(spec => { fileToOpen = fileToOpen.replace(spec, '') })
+      if (fs.existsSync(fileToOpen)) {
+        vscode.workspace.openTextDocument(vscode.Uri.file(fileToOpen))
+          .then(vscode.window.showTextDocument)
+      } else {
+        fileToOpen = `**${lastPath}${filenameWithoutExtension}${filenameExtension}`.replace('_spec', '')
+        vscode.workspace.findFiles(fileToOpen, '**/node_modules/**')
+          .then(files => {
+            vscode.workspace.openTextDocument(vscode.Uri.file(files[0].fsPath))
+              .then(vscode.window.showTextDocument)
+          })
+      }
+    }
+  })
 
-        const path = openedFile[1];
-        const lastPath = openedFile[2]
-        const filenameWithoutExtension = openedFile[3];
-        const filenameExtension = openedFile[4];
-
-        const isSpecFile = /(\.|_)(spec|test)\./
-        if (!isSpecFile.test(openedFile)) {
-            const sufixSpecs = ['.spec', '.test', '_spec', '_test']
-            
-            const sufixToOpen = sufixSpecs.map(spec => `${path}${filenameWithoutExtension}${spec}${filenameExtension}`)
-                .filter(spec => fs.existsSync(spec))
-            if (sufixToOpen.length > 0) {
-                vscode.workspace.openTextDocument(vscode.Uri.file(sufixToOpen[0]))
-                    .then(vscode.window.showTextDocument)
-            } else {
-                const fileToOpen = `**${lastPath}${filenameWithoutExtension}_spec${filenameExtension}`
-                vscode.workspace.findFiles(fileToOpen, '**/node_modules/**')
-                    .then(files => {
-                        vscode.workspace.openTextDocument(vscode.Uri.file(files[0].fsPath))
-                        .then(vscode.window.showTextDocument)
-                    })
-            }
-        } else {
-            const sufixSpecs = ['.spec', '.test', '_spec', '_test']
-            let fileToOpen = openedFilename
-            sufixSpecs.forEach(spec => { fileToOpen = fileToOpen.replace(spec, '') })
-            if (fs.existsSync(fileToOpen)) {
-                vscode.workspace.openTextDocument(vscode.Uri.file(fileToOpen))
-                    .then(vscode.window.showTextDocument)
-            } else {
-                fileToOpen = `**${lastPath}${filenameWithoutExtension}${filenameExtension}`.replace('_spec', '')
-                vscode.workspace.findFiles(fileToOpen, '**/node_modules/**')
-                    .then(files => {
-                        vscode.workspace.openTextDocument(vscode.Uri.file(files[0].fsPath))
-                        .then(vscode.window.showTextDocument)
-                    })
-            }
-        }
-    });
-
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
-exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+exports.activate = activate
+
+function deactivate () {}
+
+exports.deactivate = deactivate
